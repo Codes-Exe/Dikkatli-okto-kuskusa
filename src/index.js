df29bfb9b0f9389c07432db00224cf6ca3146e4b
Erçetin Güler <ercetinguler@hotmail.com>
2022-08-18 21:20:26 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const core = require('@actions/core');
+const utilities = require('./utilities');
+const sarif = require('./sarif');
+
+async function run() {
+  core.info('Starting HawkScan Action');
+  const inputs = utilities.gatherInputs();
+  const dockerCommand = utilities.buildDockerCommand(inputs);
+  let exitCode = 0;
+  let scanData;
+
+  // Run the scanner
+  if ( inputs.dryRun === 'true' ) {
+    core.info(`DRY-RUN MODE - The following command will not be run:`);
+    core.info(dockerCommand);
+  } else {
+    scanData = await utilities.runCommand(dockerCommand);
+    exitCode = scanData.exitCode;
+    core.debug(`Scanner exit code: ${scanData.exitCode} (${typeof scanData.exitCode})`);
+    core.debug(`Link to scan results: ${scanData.resultsLink} (${typeof scanData.resultsLink})`);
+  }
+
+  // Upload SARIF data
+  if (scanData && inputs.codeScanningAlerts === 'true' ) {
+    if (exitCode === 0 || exitCode === 42) {
+      await sarif.uploadSarif(scanData, inputs.githubToken);
+    } else {
+      core.error(`Skipping SARIF upload due to scan error.`)
+    }
+  }
+
+  process.exit(exitCode);
+}
+
+run();
